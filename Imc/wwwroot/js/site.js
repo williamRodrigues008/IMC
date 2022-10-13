e7b80f520d408eec11c7626c5e60348277e51cf0
William Rodrigues <wrsilva008@gmail.com>
2022-10-13 10:19:19 -0300

    Criação do modal Bootstrap e validação em caso de campos vazios

--- Imc/wwwroot/js/site.js
+++ Imc/wwwroot/js/site.js
@@ -2,12 +2,20 @@
     var peso = document.querySelector("#peso")
     var altura = document.querySelector("#altura")
     var imc = 0;
-     const pesoAltura = JSON.stringify({
-                "Peso": peso.value,
-         "Altura": altura.value
-            })
+    const pesoAltura = JSON.stringify({
+        "Peso": peso.value,
+        "Altura": altura.value
+    })
 
-    console.log(pesoAltura)
+    $(document).ready(function () {
+        $(".realizarCalculo").click(function () {
+            $("#modalRetonoResultado").modal("show")
+        })
+    })
+
+    if ((peso.value == "") || (altura.value == "")) {
+        return $("#mensagemImc").html("Os campos Peso e Altura não podem estar vazio")
+    }
 
     try {
 
@@ -17,15 +25,14 @@
             data: { pesoAltura: pesoAltura },
             success: function (resposta) {
                 imc = resposta.toFixed(2);
-                console.log(imc)
                 if (imc < 18.5) {
-                    $("#mensagemImc").html(`Seu IMC deu ${imc}, você está abaixo do peso`).fadein()
+                    $("#mensagemImc").html(`Seu IMC deu ${imc}, você está abaixo do peso`)
                 }
                 else if (imc >= 18.5 && imc <= 24.9) {
-                    $("#mensagemImc").html(`Seu IMC deu ${imc}, você está no peso ideal, parabéns`).fadein()
+                    $("#mensagemImc").html(`Seu IMC deu ${imc}, você está no peso ideal, parabéns`)
                 }
                 else if (imc >= 25 && imc <= 29.9) {
-                    $("#mensagemImc").html(`Seu IMC deu ${imc}, você está acima do peso`).fadein()
+                    $("#mensagemImc").html(`Seu IMC deu ${imc}, você está acima do peso`)
                 }
                 else if (imc >= 30 && imc <= 34.9) {
                     $("#mensagemImc").html(`Seu IMC deu ${imc}, você está no quadro: Obesidade I`)
@@ -40,20 +47,6 @@
             error: function (xhr, status, error) { }
         })
 
-        //await fetch("https://localhost:44379/Calcular", { method: 'POST', headers: { 'content-type': 'application/json', 'Content-Type': 'application/json; charset=utf-8' }, body: pesoAltura })
-        //    .then(function (response) {
-        //        if (response.ok) {
-        //            $(".mensagemImc").html("Seu IMC deu ")
-        //            return response.text()
-        //        }
-        //        throw new Error("Erro da requisição")
-        //    })
-        //    .then(function (text) {
-        //        console.log("Sucesso na requisição" + text)
-        //    })
-        //    .catch(function (erro) {
-        //        console.log("ERRO DE REQ " + erro)
-        //    })
     }
     catch (erro) {
         console.error(erro)
